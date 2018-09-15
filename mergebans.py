9b6fd08f32da7b5b76ec561473e77c5db4a81e4e
Patrick Seiter <patrick.seiter@gmail.com>
2018-09-15 15:27:52 -0500

    First working version of mergebans

--- mergebans.py
+++ mergebans.py
@@ -29,32 +29,32 @@ class DiffError(Exception):
 # hold the contents of the different versions of the log.txt
 # file.
 
-ancestorbans = []
-currentbans = []
-branchbans = []
+ancestorbans = {}
+currentbans = {}
+branchbans = {}
 
 # The version of the file from the common ancestor of the two branches.
 # This constitutes the 'base' version of the file.
 #ancestor = open(sys.argv[1],'r').readlines()
 with open(sys.argv[1], 'r') as csvfile:
     bans = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for row in bans:
-            ancestorbans[ ';'.join(row[1], row[2], row[5]) ] = row;
+    for row in bans:
+        ancestorbans[ ';'.join([row[1], row[2], row[5]]) ] = row
 
 # The version of the file at the HEAD of the current branch.
 # The result of the merge should be left in this file by overwriting it.
 #current = open(sys.argv[2],'r').readlines()
 with open(sys.argv[2], 'r') as csvfile:
     bans = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for row in bans:
-            currentbans[ ';'.join(row[1], row[2], row[5]) ] = row;
+    for row in bans:
+        currentbans[ ';'.join([row[1], row[2], row[5]]) ] = row
 
 # The version of the file at the HEAD of the other branch.
 #other = open(sys.argv[3],'r').readlines()
 with open(sys.argv[3], 'r') as csvfile:
     bans = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for row in bans:
-            branchbans[ ';'.join(row[1], row[2], row[5]) ] = row;
+    for row in bans:
+        branchbans[ ';'.join([row[1], row[2], row[5]]) ] = row
 
 # The merge algorithm is as follows:
 # Append any text that was added to the beginning of the file in the
@@ -62,20 +62,25 @@ with open(sys.argv[3], 'r') as csvfile:
 # If the other branch contains changes other than adding text at the
 # beginning of the file then fail.
 try:
-    combined_bans = currentbans + branchbans
+    combined_bans = {**currentbans, ** branchbans}
     sorted(combined_bans, key=lambda ban: ban[5])
-except DiffError, d:
-    iprint ''.join(difflib.unified_diff(ancestor,other))
+except DiffError:
+    print(''.join(difflib.unified_diff(ancestor,other)))
     sys.exit(1)
 
-print "The following text will be appended to the top of the file:"
-print ''.join(ancestor_to_other)
-f = open(sys.argv[2],'w')
-csvwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-csvwriter.writerows(combined_bans)
+print("The following text will be appended to the top of the file:")
+for key in combined_bans:
+    print(';'.join(combined_bans[key]))
+#print ''.join()
+#f = open(sys.argv[2],'w')
+with open(sys.argv[2], 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+    for key in combined_bans:
+        csvwriter.writerow(combined_bans[key])
+    #csvwriter.writerows(combined_bans)
 #f.writelines(ancestor_to_other)
 #f.writelines(current)
-f.close()
+#f.close()
 
 # Exit with zero status if the merge went cleanly, non-zero otherwise.
 sys.exit(0)
