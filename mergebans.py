496413f4eb5288586c7866cc8126deb652bc32f3
Patrick Seiter <patrick.seiter@gmail.com>
2018-09-23 20:51:28 -0500

    Final versions of Python files

--- mergebans.py
+++ mergebans.py
@@ -1,86 +1,114 @@
-#!/bin/env python
-"""mergelog
+#!/bin/env python3
+"""mergebans
+Copyright (c) 2018, Patrick Seiter
+All rights reserved.
 
-This is a custom merge driver for git. It should be called from git
-with a stanza in .git.config like this:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the organization nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-    [merge "mergelog"]
-        name = A custom merge driver for my log.txt file.
-        driver = ~/scripts/mergelog %O %A %B %L
-        recursive = binary
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL PATRICK SEITER BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-To make git use the custom merge driver you also need to put this in
-.git/info/attributes:
+This is a custom merge driver for git. You can either define
+it in .git/config like this:
 
-   log.txt merge=mergelog
+[merge "mergebans"]
+        name = Merge reserved slots in SCP:SL.
+        driver = python3 ./mergebans.py %O %A %B %L
 
-This tells git to use the 'mergelog' merge driver to merge files called log.txt.
+Or define it in .gitconfig and run the following command:
+
+git config --local include.path ../.gitconfig
+
+Then make sure to define it in your .gitattributes file like so:
+
+SteamIdBans.txt merge=mergebans
+IpBans.txt merge=mergebans
+
+This tells git to use the 'mergebans' merge driver.
 
 """
-import sys, difflib, csv
+import sys, csv, time
 
-class DiffError(Exception):
-    def __init__(self,value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
+TICKS_OFFSET = 621355968000000000
+MICROSECOND_TENTH = 10000000
 
 # The arguments from git are the names of temporary files that
 # hold the contents of the different versions of the log.txt
 # file.
 
-ancestorbans = {}
-currentbans = {}
-branchbans = {}
+ancestor_bans = {}
+current_bans = {}
+branch_bans = {}
 
 # The version of the file from the common ancestor of the two branches.
 # This constitutes the 'base' version of the file.
-#ancestor = open(sys.argv[1],'r').readlines()
 with open(sys.argv[1], 'r', encoding='utf-8-sig') as csvfile:
     bans = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in bans:
-        ancestorbans[ ';'.join([row[1], row[2], row[5]]) ] = row
+        ancestor_bans[ row[1] ] = row # ';'.join([row[1], row[2], row[5]])
 
 # The version of the file at the HEAD of the current branch.
 # The result of the merge should be left in this file by overwriting it.
-#current = open(sys.argv[2],'r').readlines()
 with open(sys.argv[2], 'r', encoding='utf-8-sig') as csvfile:
     bans = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in bans:
-        currentbans[ ';'.join([row[1], row[2], row[5]]) ] = row
+        current_bans[ row[1] ] = row # ';'.join([row[1], row[2], row[5]])
 
 # The version of the file at the HEAD of the other branch.
-#other = open(sys.argv[3],'r').readlines()
 with open(sys.argv[3], 'r', encoding='utf-8-sig') as csvfile:
     bans = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in bans:
-        branchbans[ ';'.join([row[1], row[2], row[5]]) ] = row
+        branch_bans[ row[1] ] = row # ';'.join([row[1], row[2], row[5]])
 
 # The merge algorithm is as follows:
 # Append any text that was added to the beginning of the file in the
 # other branch to the beginning of the current branch's copy of the file.
 # If the other branch contains changes other than adding text at the
 # beginning of the file then fail.
-try:
-    combined_bans = {**currentbans, ** branchbans}
-    combined_bans = sorted(combined_bans, key=lambda ban: int(ban[5]))
-except DiffError:
-    print(''.join(difflib.unified_diff(ancestor,other)))
-    sys.exit(1)
+combined_bans = current_bans.copy()
+now = time.time()
+ticks_now = TICKS_OFFSET + (now * MICROSECOND_TENTH)
+for key in combined_bans:
+    if combined_bans[key][2] < ticks_now:
+        combined_bans.pop(key, None)
+
+for key in branch_slots:
+    branch_diff = branch_slots[key][2] - branch_slots[key][5]
+    if key in current_slots:
+        current_diff = current_slots[key][2] - current_slots[key][5]
+        if branch_diff < current_diff:
+            combined_bans[key] = current_slots[key]
+        elif current_diff > branch_diff:
+            combined_bans[key] = branch_slots[key]
+    else:
+        combined_bans[key] = branch_slots[key]
+combined_bans = sorted(combined_bans, key=lambda ban: int(ban[5]))
 
 print("The following text will be appended to the top of the file:")
-for key in combined_bans:
+for key in branch_bans:
     print(';'.join(combined_bans[key]))
-#print ''.join()
-#f = open(sys.argv[2],'w')
+
 with open(sys.argv[2], 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
     for key in combined_bans:
         csvwriter.writerow(combined_bans[key])
-    #csvwriter.writerows(combined_bans)
-#f.writelines(ancestor_to_other)
-#f.writelines(current)
-#f.close()
 
 # Exit with zero status if the merge went cleanly, non-zero otherwise.
 sys.exit(0)
--- mergeslots.py
+++ mergeslots.py
@@ -1,7 +1,46 @@
-#!/bin/env python
+#!/bin/env python3
 """mergeslots
+Copyright (c) 2018, Patrick Seiter
+All rights reserved.
 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the organization nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL PATRICK SEITER BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This is a custom merge driver for git. You can either define
+it in .git/config like this:
+
+[merge "mergeslots"]
+        name = Merge reserved slots in SCP:SL.
+        driver = python3 ./mergeslots.py %O %A %B %L
+
+Or define it in .gitconfig and run the following command:
+
+git config --local include.path ../.gitconfig
+
+Then make sure to define it in your .gitattributes file like so:
+
+Reserved\ Slots.txt merge=mergeslots
+
+This tells git to use the 'mergeslots' merge driver.
 
 """
 import sys, csv
@@ -16,7 +55,6 @@ branch_slots = {}
 
 # The version of the file from the common ancestor of the two branches.
 # This constitutes the 'base' version of the file.
-#ancestor = open(sys.argv[1],'r').readlines()
 with open(sys.argv[1], 'r', encoding='utf-8') as csvfile:
     slots = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in slots:
@@ -27,46 +65,45 @@ with open(sys.argv[1], 'r', encoding='utf-8') as csvfile:
 
 # The version of the file at the HEAD of the current branch.
 # The result of the merge should be left in this file by overwriting it.
-#current = open(sys.argv[2],'r').readlines()
 with open(sys.argv[2], 'r', encoding='utf-8') as csvfile:
     slots = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in slots:
         if len(row) == 1:
-            ancestor_slots[ row[0] ] = row
+            current_slots[ row[0] ] = row
         else
-            ancestor_slots[ row[1] ] = row
+            current_slots[ row[1] ] = row
 
 # The version of the file at the HEAD of the other branch.
-#other = open(sys.argv[3],'r').readlines()
 with open(sys.argv[3], 'r', encoding='utf-8') as csvfile:
     slots = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in slots:
         if len(row) == 1:
-            ancestor_slots[ row[0] ] = row
+            branch_slots[ row[0] ] = row
         else
-            ancestor_slots[ row[1] ] = row
+            branch_slots[ row[1] ] = row
 
 # The merge algorithm is as follows:
 # Append any text that was added to the beginning of the file in the
 # other branch to the beginning of the current branch's copy of the file.
 # If the other branch contains changes other than adding text at the
 # beginning of the file then fail.
-combined_slots = {**current_slots, **branch_slots}
-combined_slots = sorted(combined_slots, key=lambda ban: int(ban[5]))
+combined_slots = current_slots.copy()
+for key in branch_slots:
+    if key in current_slots:
+        if len(branch_slots[key]) < len(current_slots[key]):
+            combined_slots[key] = current_slots[key]
+        else:
+            combined_slots[key] = branch_slots[key]
+    else:
+        combined_slots[key] = branch_slots[key]
 
 print("The following text will be appended to the top of the file:")
 for key in combined_slots:
     print(';'.join(combined_slots[key]))
-#print ''.join()
-#f = open(sys.argv[2],'w')
 with open(sys.argv[2], 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
     for key in combined_slots:
         csvwriter.writerow(combined_slots[key])
-    #csvwriter.writerows(combined_slots)
-#f.writelines(ancestor_to_other)
-#f.writelines(current)
-#f.close()
 
 # Exit with zero status if the merge went cleanly, non-zero otherwise.
 sys.exit(0)
