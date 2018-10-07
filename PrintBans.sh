6ee048746afb4ec022bb1919ab4f0bf50546ea0b
Patrick Seiter <patrick.seiter@gmail.com>
2018-10-07 01:15:51 -0500

    Actually fix PrintBans r in arithmetic

--- PrintBans.sh
+++ PrintBans.sh
@@ -1,36 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
 epoch_ticks=621355968000000000
 ticks_per_second=10000000
 date_string=""
 
 ticksToDateString() {
+	if [[ -z $1 ]]
+	then
+		date_string=INVALID
+		return 1
+	fi
 	ticks_since_epoch=$(($1 - $epoch_ticks))
 	seconds_since_epoch=$(($ticks_since_epoch / $ticks_per_second))
+	if [[ $seconds_since_epoch -gt 2147483647 ]] && uname -m | grep -qv _64
+	then
+		date_string=PERMANENT
+		return 50
+	fi
 	date_string=`date -d @$seconds_since_epoch "+%Y-%m-%d %H:%M:%S"`
 }
 
+separator=","
 for file in SteamIdBans.txt IpBans.txt
 do
 	echo Name,Steam Profile,Start Time,End Time,Admin,Reason
-	while read ban
+	while IFS= read -r ban || [[ $ban ]]
 	do
-		name=$(echo $ban | cut -d';' -f1)
-		name=${name/,/\\,}
-		idip=$(echo $ban | cut -d';' -f2)
-		end_time=$(echo $ban | cut -d';' -f3)
-		ticksToDateString ${end_time:-0}
-		end_time=$date_string
-		reason=$(echo $ban | cut -d';' -f4)
-		admin=$(echo $ban | cut -d';' -f5)
-		start_time=$(echo $ban | cut -d';' -f6)
-		ticksToDateString ${start_time:-0}
-		start_time=$date_string
+		ban=${ban%$'\r'}
+		IFS=';' read -ra row <<< "$ban"
+		#name=$(echo $ban | cut -d';' -f1)
+		#name=${name/,/\\,}
+		#idip=$(echo $ban | cut -d';' -f2)
+		#end_time=$(echo $ban | cut -d';' -f3)
+		#ticksToDateString $end_time
+		#end_time=$date_string
+		#reason=$(echo $ban | cut -d';' -f4)
+		#admin=$(echo $ban | cut -d';' -f5)
+		#start_time=$(echo $ban | cut -d';' -f6)
+		#ticksToDateString $start_time
+		#start_time=$date_string
+		row[0]=${row[0]//,/\\,}
 		if [ "$file" = "SteamIdBans.txt" ]
 		then
-			idip=http://steamcommunity.com/profiles/$idip
+			row[1]=http://steamcommunity.com/profiles/${row[1]}
 		fi
-		echo $name,$idip,$start_time,$end_time,$admin,$reason
-	done <$file
+		ticksToDateString ${row[2]}
+		row[2]=$date_string
+		ticksToDateString ${row[5]}
+		row[5]=$date_string
+		echo ${row[0]}$separator${row[1]}$separator${row[5]}$separator${row[2]}$separator${row[4]}$separator${row[3]}
+	done<"$file"
 	echo ""
 done
 
